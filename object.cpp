55ab6ff306ac6e163468e301a681c60bd32185f2
Johannes <johannestammerand@gmail.com>
2024-04-28 22:13:02 +0300

    velocity

--- object.cpp
+++ object.cpp
@@ -3,19 +3,34 @@
 
 using namespace std;
 
-void Object::applyGravity(){
-    y = fmax(y - g, 0.0);
+void Object::applyVelocity(){
+    if (y != 0){
+        velocityUp -= g;
+    }
+
+    y = fmax(y + velocityUp, 0.0);
+    x = x + velocitySide;
+
+    if (y == 0) {
+        velocityUp = 0;
+    }
+
+    if (velocitySide > 0){
+        velocitySide = fmin(velocitySide - 0.2, 0.0);
+    } else {
+        velocitySide = fmax(velocitySide + 0.2, 0.0);
+    }
 }
 
-void Object::push(int f){
-    x += f;
+void Object::pushSide(float f){
+    velocitySide += f;
+}
+
+void Object::pushUp(float f){
+    velocityUp += f;
 }
 
 int* Object::getPos(){
     static int p[2] = {x, y};
     return p;
-}
-
-ostream&& operator<<(ostream& os, Object o){
-    os << 
 }
--- object.h
+++ object.h
@@ -6,18 +6,26 @@ using namespace std;
 
 class Object{
 public:
-    Object () : y{0}, x{0}, g{0} {};
+    Object () : y{0}, x{0}, g{0}, velocityUp{0}, velocitySide{0} {};
     Object (float ny, float nx, float ng) : y{ny}, x{nx}, g{ng} {};
 
-    void applyGravity();
-    void push(int f);
+    void applyVelocity();
+    void pushSide(float f);
+    void pushUp(float f);
     
     int* getPos();
-    friend ostream& operator<<(ostream& os, Object o);
+    friend ostream& operator<<(ostream& os, const Object o);
 private:
+    float velocityUp;
+    float velocitySide;
     float y;
     float x;
     float g;
 };
+
+ostream& operator<<(ostream& os, Object o){
+    os << "(" << o.x << ", " << o.y << ")" << endl;
+    return os; 
+}
 
 #endif
